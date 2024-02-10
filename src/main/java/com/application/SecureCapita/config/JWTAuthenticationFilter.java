7feff5b638ca8940beef3e0a1b89a1eef31fa1d6
Tyrien Gilpin <gilpinedu@gmail.com>
2024-02-09 23:02:27 -0500

    jwt implementation

--- src/main/java/com/application/SecureCapita/config/JWTAuthenticationFilter.java
+++ src/main/java/com/application/SecureCapita/config/JWTAuthenticationFilter.java
@@ -1,4 +1,38 @@
 package com.application.SecureCapita.config;
 
-public class JWTAuthenticationFilter {
+import com.application.SecureCapita.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Service //extends the @Component annotation
+//@Controller
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail; //which is username
+        if(authHeader == null || authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+    }
 }
