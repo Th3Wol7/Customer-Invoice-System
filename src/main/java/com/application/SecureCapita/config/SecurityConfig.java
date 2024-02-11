b2c290c96bd6dada26f50e163fe94edfb6785fd0
Tyrien Gilpin <gilpinedu@gmail.com>
2024-02-11 09:46:07 -0500

    auth config

--- src/main/java/com/application/SecureCapita/config/SecurityConfig.java
+++ src/main/java/com/application/SecureCapita/config/SecurityConfig.java
@@ -9,18 +9,42 @@ package com.application.SecureCapita.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    private final JWTAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
-        return null;
+    //Whitelist - endpoints that doesn't require authentication or open tokens
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF configuration
+        http.csrf(CsrfConfigurer::disable);
+
+        // Authorization configuration
+        http.authorizeHttpRequests(authorization -> {
+            authorization.requestMatchers("").permitAll();
+            authorization.anyRequest().authenticated();
+        });
+
+        // Session management configuration
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // Authentication provider configuration
+        http.authenticationProvider(authenticationProvider);
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 
 }
